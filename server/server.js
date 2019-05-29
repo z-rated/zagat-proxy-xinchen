c7b4692a43d88bd12805cf4052c2c3cd3298bcb6
eugenia ong <eugenia.osy@gmail.com>
2019-05-29 11:24:22 -0700

    Wrap info and reviews in div

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const port = 1000;
-// app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
